845295cd57fafda4198aece29a518bd1d352bebf
xNovax <nova1104@live.com>
2015-05-31 17:04:06 -0400

    Set up initial UI

--- Fortitude/Program.cs
+++ Fortitude/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Fortitude
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private char[] allCharacters =
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '<', '>', '?'
+            };
+
+        private static void Main()
         {
+            InitializeProgram();
+            string realPass = "password123";
+
+            Console.ReadLine();
+        }
+
+        public static void MakePassword()
+        {
+            
+        }
+
+        public static void CheckPassword()
+        {
+        }
+
+        public static void InitializeProgram()
+        {
+            Console.Title = ("Fortitude");
+            DrawSeparator();
+            Console.WriteLine("Welcome to Fortitude!");
+            DrawSeparator();
+        }
+
+        public static void DrawSeparator()
+        {
+            var worker = new Thread(DrawSeparatorWork);
+            worker.Start();
+            worker.Join();
+        }
+
+        private static void DrawSeparatorWork()
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("=");
+            }
         }
     }
 }
