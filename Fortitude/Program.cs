f01cba97b4c5116c072eab2e41e5d86661d224c2
xNovax <nova1104@live.com>
2015-06-01 15:03:14 -0400

    Program works but is very very slow and resource intensive

--- Fortitude/Program.cs
+++ Fortitude/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace Fortitude
 {
     internal class Program
     {
-        private const string realPass = "password123";
-        private static Random gen = new Random();
-        private static DateTime timeStarted = DateTime.Now;
+        private const string realPass = "123";
+        private static readonly DateTime timeStarted = DateTime.Now;
         private static string foundPassword;
+        private static Boolean passFound = false;
 
 
 
-        private static readonly char[] allCharacters =
+        private static readonly string[] allCharacters =
         {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '<', '>', '?'
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+            "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7",
+            "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "?"
         };
 
         private static readonly List<Thread> threads = new List<Thread>();
+        private static readonly List<string> threadStartChar = new List<string>(); 
 
         private static void Main()
         {
@@ -34,28 +36,35 @@ namespace Fortitude
         {
             for (int i = 0; i < allCharacters.Length; i++)
             {
-                var newThread = new Thread(FindPasswordMask) {Name = Convert.ToString(i)};
+                var newThread = new Thread(FindPasswordMask);
+                newThread.Name = i.ToString();
                 threads.Add(newThread);
+                threadStartChar.Add(allCharacters[i]);
                 threads[i].Start();
             }
         }
 
         public static void FindPasswordMask()
         {
-            string x = Convert.ToString(allCharacters[Convert.ToInt32(Thread.CurrentThread.Name)]);
-            FindPasswordRecurse(x);
+            string x = allCharacters[Convert.ToInt32(Thread.CurrentThread.Name)];
+            FindPasswordWork(x);
         }
 
-        public static void FindPasswordRecurse(string x)
+        public static void FindPasswordWork(string x)
         {
             if (CheckPassword(x))
             {
                 Console.WriteLine("Time to Find: " + DateTime.Now.Subtract(timeStarted) + " seconds");
                 Console.WriteLine("Password = " + x);
+                passFound = true;
             }
-            else
+            else if(passFound == false)
             {
-                
+                for (int i = 0; i < allCharacters.Count(); i++)
+                {
+                    string temp = x + allCharacters[i];
+                    FindPasswordWork(temp);
+                }
             }
         }
 
